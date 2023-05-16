53eaa9fedfa03017a8685863b45d3bf84789b4d2
Gigantosaurio <jalquezar04@gmail.com>
2023-05-16 18:19:40 +0200

    carrito casi working

--- build/web/js/shopcart.js
+++ build/web/js/shopcart.js
@@ -1,10 +1,9 @@
 listar_json();
 
 //LISTADO DE LOS PRODUCTOS EN BASE A UN JSON DE PRUEBA
-function listar_json(){
-    fetch('js/json_capsulas.json')
-    .then(response => response.json())
-    .then(data => {
+async function listar_json(){
+      var items = await filtrarProductos()
+      console.log(items)
       // Ahora, los datos del archivo JSON están almacenados en la variable 'data'.
       const listado = document.querySelector('.items')
       //Variable que usaremos para obtener subtotal, shipping y el.
@@ -13,7 +12,9 @@ function listar_json(){
       var shipping = 0.00;
       var total = 0.00;
       // Recorremos el JSON con un forEach para ir creando y listando cada elemento.
-      data.forEach(item => {
+      console.log(items.length)
+      for (let i = 0; i < items.length; i++) {
+        const item = items[i];
         total_item = item.precio * item.cantidad;
         const producto = document.createElement('div');
         producto.classList.add('item');
@@ -25,11 +26,11 @@ function listar_json(){
                                 </div>
                                 <div class="item_total">` + total_item.toFixed(2) +`€</div>
                                 <img src="./IMG/logos/delete_button.png" class="delete_button" onclick="delete_item(`+ item.id +`)">
-                            <div class="hr_items"></div>`;
+                              <div class="hr_items"></div>`;
         listado.appendChild(producto);
         subtotal += parseFloat(total_item.toFixed(2));
         console.log(subtotal.toFixed(2));
-      });
+      }
       //Mostramos el subtotal.
       document.getElementById("subtotal_value").textContent = subtotal.toFixed(2) + "€";
       //Mostramos el shipping.
@@ -40,7 +41,6 @@ function listar_json(){
       total = shipping + subtotal;
       console.log("Total -> " + total);
       document.getElementById("total_div").textContent = total.toFixed(2) + "€";
-    })
 }
 
 
@@ -48,7 +48,16 @@ function listar_json(){
 function delete_item(id){
     item = document.getElementById(id);
     item.remove();
+    var productos = JSON.parse(localStorage.getItem('productos'));
+    var index = productos.indexOf(id);
+    if (index !== -1) {
+      productos.splice(index, 1);
+      localStorage.setItem('productos', JSON.stringify(productos));
+    }
 }
+
+
+
 
 /**************************************************************/
 
@@ -63,5 +72,11 @@ async function obtenerResultados() {
   });
   const resultados = await response.json();
   return resultados;
+}
+
+async function filtrarProductos() {
+  const resultados = await obtenerResultados();
+  const productosFiltrados = resultados.filter(producto => productos.includes(producto.id));
+  return productosFiltrados;
 }
 
